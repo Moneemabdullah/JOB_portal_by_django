e394faa3b4cba217858d2e791d77275421ba252b
moneem <moneem.all.abdullah@gmail.com>
2025-02-22 00:47:12 +0600

    dsfs

--- job/settings.py
+++ job/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'account',
     'jobapp.apps.JobappConfig',
     'account.apps.AccountConfig',
     # 'bootstrap5',
