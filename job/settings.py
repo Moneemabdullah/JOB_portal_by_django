276d8d37eab627352e75ab4a0327f32bafd67389
moneem <moneem.all.abdullah@gmail.com>
2025-02-22 00:42:46 +0600

    gap fixed

--- job/settings.py
+++ job/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'account',
     'jobapp.apps.JobappConfig',
     'account.apps.AccountConfig',
     # 'bootstrap5',
